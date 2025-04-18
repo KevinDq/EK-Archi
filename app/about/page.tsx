439af6ad3702a872dab3631412b878f5c63a1475
KevinDq <108054122+KevinDq@users.noreply.github.com>
2025-04-18 13:05:19 +0200

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 export default function AboutPage() {
   return (
     <main className="text-gray-800 h-screen w-screen bg-stone-100 py-16 px-4 flex flex-col gap-6 bg-repeat lg:flex-row xl:items-center">      
-      <section className="max-w-4xl mx-auto animate-fade-in-up mt-4 lg:order-2">
+      <section className="max-w-4xl mx-auto animate-fade-in-up mt-20 sm:mt-4 lg:order-2">
         <h1 className="text-5xl font-semibold text-center mb-9 mt-5 font-leah text-gray-900">
           Qui je suis
         </h1>
-        <article className="flex flex-col xl:flex-row items-center gap-12 mb-12">
-          <div className="w-60 h-96 flex justify-center rounded-full shadow-xl  border-4 border overflow-hidden ">
+        <article className="flex flex-col xl:flex-row items-center gap-12 sm:mb-12">
+          <div className="w-40 sm:w-60 sm:h-96 flex justify-center rounded-full shadow-xl  border-4 border overflow-hidden ">
             <Image
               src="/images/portrait.jpg"
               alt="Portrait de l'architecte"
@@ -18,7 +18,7 @@ export default function AboutPage() {
               className="object-cover w-full hover:scale-105 transition duration-300"
             />
           </div>
-          <div className="w-full lg:w-2/3 space-y-5 text-lg flex flex-col items-center">
+          <div className="w-full lg:w-2/3 space-y-5 text-sm sm:text-md sm:text-lg flex flex-col items-center text-justify">
             <p>
               <strong>Emilie Kinnoo</strong>, architecte passionnée, je vous accompagne
               depuis plus de 10 ans dans la réalisation de projets sur-mesure.
@@ -37,8 +37,8 @@ export default function AboutPage() {
             </Link>            
           </div>
         </article>
-        <article className="shadow-md px-8 py-10 mb-10 bg-gray-600 forced-border">          
-          <p className="text-gray-200 text-lg leading-relaxed font-serif">
+        <article className="hidden sm:block shadow-md p-3 sm:px-8 sm:py-10 mb-10 bg-gray-600 forced-border">          
+          <p className="text-gray-200 text-sm sm:text-lg leading-relaxed font-serif">
             Chaque projet est une rencontre, une histoire à façonner.
             L’architecture est ici envisagée comme un dialogue entre l’usage, la
             lumière, les matériaux et le paysage. Je place la relation client
@@ -48,7 +48,7 @@ export default function AboutPage() {
         </article>
         
       </section> 
-      <section className="flex justify-center animate-fade-in-up mt-8 xl:w-1/3">
+      <section className="flex justify-center animate-fade-in-up sm:mt-8 xl:w-1/3">
           <article className="flex flex-col space-y-4 text-center">
             <div className="bg-stone-300 p-4 shadow-lg hover:shadow-2xl transition duration-300 hover:translate-x-4">
               <div className="text-4xl mb-2">🏡</div>
@@ -67,7 +67,7 @@ export default function AboutPage() {
             </div>
           </article>
       </section>     
-      <section className="flex justify-center animate-fade-in-up mt-6 lg:order-3 xl:w-1/3">
+      <section className="flex justify-center animate-fade-in-up sm:mt-6 lg:order-3 xl:w-1/3">
         <article className="flex flex-col space-y-4 text-center">          
           <div className="bg-stone-300 p-4 shadow-lg hover:shadow-2xl transition duration-300 hover:-translate-x-4">
             <div className="text-4xl mb-2">🏗️</div>
